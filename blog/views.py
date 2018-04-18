19c9143c382582912769ab8c84ebb78da81702d9
Long Le <lelong03@gmail.com>
2018-04-18 08:29:11 +0700

    listing post and view post detail

--- blog/views.py
+++ blog/views.py
@@ -5,20 +5,27 @@ from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from django_blog import settings
 from django.views.generic import ListView
+from taggit.models import Tag
+from django.db.models import Count
 
 
 # Create your views here.
-def post_list(request):
+def post_list(request, tag_slug=None):
     object_list = Post.published.all()
-    paginator = Paginator(object_list, 2)
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+    paginator = Paginator(object_list, 3)
     page = request.GET.get('page')
+
     try:
         posts = paginator.page(page)
     except PageNotAnInteger:
         posts = paginator.page(1)
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
-    return render(request, 'blog/post/list.html', {'posts': posts, 'page': page})
+    return render(request, 'blog/post/list.html', {'posts': posts, 'page': page, 'tag': tag})
 
 
 def post_detail(request, year, month, day, post):
@@ -36,7 +43,12 @@ def post_detail(request, year, month, day, post):
             new_comment.save()
     else:
         comment_form = CommentForm()
-    return render(request, 'blog/post/detail.html', {'post': post, 'comments':comments, 'comment_form': comment_form})
+    tag_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=tag_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments,
+                                                     'comment_form': comment_form,
+                                                     'similar_posts': similar_posts})
 
 
 class PostListView(ListView):
